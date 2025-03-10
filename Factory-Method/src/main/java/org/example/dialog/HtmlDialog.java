a2a511fb9179245604f0396752c352935efc710c
DonRodrigo04 <159784968+DonRodrigo04@users.noreply.github.com>
2025-03-10 17:02:18 +0100

    Initial commit

---
+++ Factory-Method/src/main/java/org/example/dialog/HtmlDialog.java
@@ -0,0 +1,15 @@
+package org.example.dialog;
+
+import org.example.buttons.Button;
+import org.example.buttons.HtmlButton;
+
+/**
+ * HTML Dialog will produce HTML buttons.
+ */
+public class HtmlDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new HtmlButton();
+    }
+}
